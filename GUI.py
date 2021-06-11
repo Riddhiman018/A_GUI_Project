e2d482af43486bf28eb477c5eb29d35ab00622b3
Riddhiman <shubro17@outlook.com>
2021-06-11 20:16:20 +0530

    Test File

---
+++ GUI.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+class Application(tk.Frame):
+
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.Button_1 = tk.Button(self)
+        self.Button_1["text"] = "Hello There\nClick"
+        self.Button_1["fg"] = "RED"
+        self.Button_1["command"] = self.say_hi
+        self.Button_1.pack(side="top")
+        self.Button_1.pack(side="bottom")
+
+    def say_hi(self):
+        print("Hello There")
+
+
+root = tk.Tk()
+app = Application(master = root)
+app.mainloop()
