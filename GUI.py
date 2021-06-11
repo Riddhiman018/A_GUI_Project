d42b6a7983f67a0db360b25075dee35fe9966543
Riddhiman <shubro17@outlook.com>
2021-06-11 21:27:02 +0530

    Added TextEntryFeature

--- GUI.py
+++ GUI.py
@@ -7,6 +7,8 @@ class Application(tk.Frame):
         self.master = master
         self.pack()
         self.create_widgets()
+        self.entry_field = tk.Entry(self,width = 50,borderwidth=5,bg="RED")
+        self.entry_field.pack()
 
     def create_widgets(self):
         self.Button_1 = tk.Button(self)
@@ -14,11 +16,20 @@ class Application(tk.Frame):
         self.Button_1["fg"] = "RED"
         self.Button_1["command"] = self.say_hi
         self.Button_1.pack(side="top")
-        self.Button_1.pack(side="bottom")
+        self.new_button = tk.Button(self)
+        self.new_button["text"] = "Enter the function"
+        self.new_button["command"] = self.take_entry
+        self.new_button.pack()
 
     def say_hi(self):
-        myLabel = tk.Label(self,text = "Hello World")
-        myLabel.pack(side = "top")
+        self.myLabel = tk.Label(self,text = "Hello World")
+        self.myLabel.pack(side = "top")
+    
+    def take_entry(self):
+        self.entered_text = self.entry_field.get()
+        self.new_label = tk.Label(self,text=self.entered_text)
+        self.new_label.pack()
+    
 
 root = tk.Tk()
 app = Application(master = root)
